4150867eb027433eec17f68a0cbd07e0ea77d606
theoderic <onipetheoderic@gmail.com>
2021-02-21 14:38:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, createContext, useReducer } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from '../src/Pages/Home';
+import Destination from '../src/Pages/Destination';
+import SinglePipeline from '../src/Pages/SinglePipeline';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/destination" component={Destination} exact />
+        <Route path="/single_pipeline/:id" component={SinglePipeline} exact />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
